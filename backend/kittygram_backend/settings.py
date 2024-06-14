5a9bf5587d791639e20c283c49b87905956309f4
alxlen <lenko29@gmail.com>
2024-06-14 05:20:58 +0300

    E_5.1.2_review settings halfback

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,8 +11,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY',
     'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$')
 
-DEBUG = False
-# DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'alxlen.ru']
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
